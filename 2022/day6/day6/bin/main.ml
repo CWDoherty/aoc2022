becb99154b5c9d567f011fef2cabc2eaa30d7b3e
Christopher Doherty <christopher.doherty@mlb.com>
2022-12-06 13:19:09 -0500

    cleaning up day 6

--- 2022/day6/day6/bin/main.ml
+++ 2022/day6/day6/bin/main.ml
@@ -10,30 +10,24 @@ let build_list string startIndex size =
     done;
     !list;;
 
+let find_index string number_unique =
+    let currentIndex = ref 0 in
+    try
+    	while true do
+            let charSet = CharSet.elements (CharSet.of_list (build_list string !currentIndex number_unique)) in
+            let charSetString = Base.String.of_char_list charSet in
+
+            if (phys_equal (String.length charSetString) number_unique)
+            then raise Exit
+            else currentIndex := !currentIndex + 1
+    	done;
+    with
+    	Exit -> Out_channel.output_string stdout (string_of_int (!currentIndex + number_unique));;
+
 (* Part 1 *)
-let currentIndex = ref 0 in
-try
-    while true do
-        let charSet = CharSet.elements (CharSet.of_list (build_list input !currentIndex 4)) in
-        let charSetString = Base.String.of_char_list charSet in
+find_index input 4;;
 
-        if (phys_equal (String.length charSetString) 4)
-        then raise Exit
-        else currentIndex := !currentIndex + 1
-    done;
-with Exit -> Out_channel.output_string stdout (string_of_int (!currentIndex + 4));
-
-print_endline "\n";;
-
+print_endline "";;
 (* Part 2 *)
-let currentIndexPart2 = ref 0 in
-try
-    while true do
-        let charSet = CharSet.elements (CharSet.of_list (build_list input !currentIndexPart2 14)) in
-        let charSetString = Base.String.of_char_list charSet in
+find_index input 14;;
 
-        if (phys_equal (String.length charSetString) 14)
-        then raise Exit
-        else currentIndexPart2 := !currentIndexPart2 + 1
-    done;
-with Exit -> Out_channel.output_string stdout (string_of_int (!currentIndexPart2 + 14));
