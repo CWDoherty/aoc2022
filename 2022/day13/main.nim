92f672a16201c080aa7ec78c60132918b2a1f001
Christopher Doherty <christopher.doherty@mlb.com>
2022-12-13 21:44:24 -0500

    day 13 part 2

--- 2022/day13/main.nim
+++ 2022/day13/main.nim
@@ -1,5 +1,6 @@
 import json
 import sequtils
+import std/algorithm
 import std/strformat
 import std/strutils
 import sugar
@@ -8,7 +9,7 @@ let pairs = collect(newSeq):
   for line in readAll(open("input.txt")).strip().split("\n\n"):
     map(line.split("\n"), proc(l: string): JsonNode = parseJson(l))
 
-proc isInOrder(left: JsonNode, right: JsonNode): bool =
+proc isInOrder(left, right: JsonNode): bool =
   if (left.kind == JInt and right.kind == JInt):
     let leftInt = left.getInt()
     let rightInt = right.getInt()
@@ -36,11 +37,32 @@ proc isInOrder(left: JsonNode, right: JsonNode): bool =
     let rightJson = newJInt(len(rightElems))
     return isInOrder(leftJson, rightJson)
 
-var sumInOrder = 0
+var part1 = 0
 for i in 1..len(pairs):
   let pair = pairs[i - 1]
   if (isInOrder(pair[0], pair[1])):
-    sumInOrder += i
+    part1 += i
 
+echo fmt"Part 1: {part1}"
 
-echo fmt"Part 1: {sumInOrder}"
+let lines = collect(newSeq):
+  for line in filter(readAll(open("input.txt")).strip().split("\n"), proc(l: string): bool = l != ""):
+    parseJson(line)
+
+proc compare(left, right: JsonNode): int =
+  if (left == right):
+    return 0
+  elif (isInOrder(left, right)):
+    return -1
+  else:
+    return 1
+
+let dividerPacketTwo = parseJson("[[2]]")
+let dividerPacketSix = parseJson("[[6]]")
+var part2Input = concat(lines, @[dividerPacketTwo], @[dividerPacketSix])
+part2Input.sort(compare)
+let twoIndex = find(part2Input, dividerPacketTwo) + 1
+let sixIndex = find(part2Input, dividerPacketSix) + 1
+let part2 = twoIndex * sixIndex
+
+echo fmt"Part 2: {part2}"
